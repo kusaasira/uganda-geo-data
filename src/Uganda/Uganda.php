5ab5476d0ad7d3c01b97085e29972da2af0ad15b
Zack <zack@zacklott.co.uk>
2022-10-29 20:46:44 +0100

    Implement the ability to generate the tree structure on creation

--- src/Uganda/Uganda.php
+++ src/Uganda/Uganda.php
@@ -148,4 +148,100 @@ final class Uganda
 
         throw new VillageNotFoundException();
     }
+
+    /** @return array<string, District> */
+    private function generateTree(): array
+    {
+        /** @var array<int, array<string, string>> $villages */
+        $villages = $this->getFileContents(__DIR__ . '/Data/villages.json');
+
+        /** @var array<int, array<string, string>> $parishes */
+        $parishes = $this->getFileContents(__DIR__ . '/Data/parishes.json');
+
+        /** @var array<int, array<string, string>> $subCounties */
+        $subCounties = $this->getFileContents(__DIR__ . '/Data/sub_counties.json');
+
+        /** @var array<int, array<string, string>> $counties */
+        $counties = $this->getFileContents(__DIR__ . '/Data/counties.json');
+
+        /** @var array<int, array<string, string>> $districts */
+        $districts = $this->getFileContents(__DIR__ . '/Data/districts.json');
+
+        /** @var array<string, array<string, Village>> $mappedVillages */
+        $mappedVillages = [];
+        foreach ($villages as $village) {
+            $mappedVillages[$village['parish']][strtolower($village['name'])] = new Village(
+                (int) $village['id'],
+                $village['name'],
+                (int) $village['parish']
+            );
+        }
+
+        /** @var array<string, array<string, Parish>> $mappedParishes */
+        $mappedParishes = [];
+        foreach ($parishes as $parish) {
+            if (!isset($parish['name'])) {
+                var_dump($parish);
+                die();
+            }
+            $mappedParishes[$parish['subcounty']][strtolower($parish['name'])] = new Parish(
+                (int) $parish['id'],
+                ucfirst($parish['name']),
+                (int) $parish['subcounty'],
+                $mappedVillages[$parish['id']] ?? []
+            );
+        }
+
+        /** @var array<string, array<string, SubCounty>> $mappedSubCounties */
+        $mappedSubCounties = [];
+        foreach ($subCounties as $subCounty) {
+            $mappedSubCounties[$subCounty['county']][strtolower($subCounty['name'])] = new SubCounty(
+                (int) $subCounty['id'],
+                ucfirst($subCounty['name']),
+                (int) $subCounty['county'],
+                $mappedParishes[$subCounty['id']] ?? []
+            );
+        }
+
+        /** @var array<string, array<string, County>> $mappedCounties */
+        $mappedCounties = [];
+        foreach ($counties as $county) {
+            $mappedCounties[$county['district']][strtolower($county['name'])] = new County(
+                (int) $county['id'],
+                ucfirst($county['name']),
+                (int) $county['district'],
+                $mappedSubCounties[$county['id']] ?? []
+            );
+        }
+
+        /** @var array<string, District> $mappedDistrict */
+        $mappedDistrict = [];
+        foreach ($districts as $district) {
+            $mappedDistrict[strtolower($district['name'])] = new District(
+                (int) $district['id'],
+                ucfirst($district['name']),
+                $mappedCounties[$district['id']] ?? []
+            );
+        }
+
+        return $mappedDistrict;
+    }
+
+    /** @return array<string, string> */
+    private function getFileContents(string $filePath): array
+    {
+        try {
+            /** @var array<string, string> $response */
+            $response = (array) json_decode(
+                (string) file_get_contents($filePath),
+                true,
+                512,
+                JSON_THROW_ON_ERROR
+            );
+        } catch (\JsonException $jsonException) {
+            return [];
+        }
+
+        return $response;
+    }
 }
